47124cc0c11ce1118738536d4d18c8086fd968ff
Ryan Betts <ryan.betts@student.swic.edu>
2019-10-02 16:46:56 -0500

    Listen for a submit event on the form

--- scripts.js
+++ scripts.js
@@ -24,8 +24,14 @@ function divide(a, b) {
 function remainder(a, b) {
   return a % b;
 }
+// use an anonymous callback function as the 2nd argument
 const form = document.querySelector("form");
-
+const num1 = document.querySelector("num-1");
+const num2 = document.querySelector("num-2");
+const select = document.querySelector("select");
 form.addEventListener("submit", function(event) {
+  // Prevent the default browser behavior
   event.preventDefault();
+  // form inputs have a property called value
+  console.log(num1.value, num2.value, select.value);
 });
