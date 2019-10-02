d2f95ce430efc3ead8e0ce28a8cdcb47d2986eb0
Ryan Betts <ryan.betts@student.swic.edu>
2019-10-02 18:37:22 -0500

    add result of addition to the output tag.

--- scripts.js
+++ scripts.js
@@ -26,12 +26,21 @@ function remainder(a, b) {
 }
 // use an anonymous callback function as the 2nd argument
 const form = document.querySelector("form");
-const num1 = document.querySelector("num-1");
-const num2 = document.querySelector("num-2");
+const num1 = document.querySelector("#num-1");
+const num2 = document.querySelector("#num-2");
 const select = document.querySelector("select");
+const output = document.querySelector("output");
 form.addEventListener("submit", function(event) {
   // Prevent the default browser behavior
   event.preventDefault();
+
+  console.log(add(num1.value, num2.value));
+
+  // Display the result
+  // Get the values in as the arguments to the function
+  // ToDo: Figure out how to dynamically call a function by name
   // form inputs have a property called value
+  output.textContent = add(num1.value, num2.value, select.value);
+
   console.log(num1.value, num2.value, select.value);
 });
