ff64c9f8113bdbdc2d92aa4a62f562dbb7a2d534
Ryan Betts <ryan.betts@student.swic.edu>
2019-10-02 16:36:31 -0500

    Add form markup and grab 'form' element from the page.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,31 @@
-// Enter a description of this program here.
+// Simple arithmetic calc app
 
 /**
  * Write some pseudo code steps here.
  * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
  */
+
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function remainder(a, b) {
+  return a % b;
+}
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+});
