8309fa2f0d9624c6729177d49eb9b439f5bed904
Ryan Betts <ryan.betts@student.swic.edu>
2019-10-02 19:21:01 -0500

    Move functions into calc object.

--- scripts.js
+++ scripts.js
@@ -5,25 +5,24 @@
  * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
  */
 
-function add(a, b) {
-  return Number(a) + Number(b);
-}
+const calc = {
+  add: function add(a, b) {
+    return Number(a) + Number(b);
+  },
+  subtract: function subtract(a, b) {
+    return a - b;
+  },
+  multiply: function multiply(a, b) {
+    return a * b;
+  },
+  divide: function divide(a, b) {
+    return a / b;
+  },
+  remainder: function remainder(a, b) {
+    return a % b;
+  }
+};
 
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function remainder(a, b) {
-  return a % b;
-}
 // use an anonymous callback function as the 2nd argument
 const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
@@ -34,13 +33,13 @@ form.addEventListener("submit", function(event) {
   // Prevent the default browser behavior
   event.preventDefault();
 
-  console.log(add(num1.value, num2.value));
+  console.log(calc.add(num1.value, num2.value));
 
   // Display the result
   // Get the values in as the arguments to the function
   // ToDo: Figure out how to dynamically call a function by name
   // form inputs have a property called value
-  output.textContent = add(num1.value, num2.value, select.value);
+  output.textContent = calc.add(num1.value, num2.value, select.value);
 
   console.log(num1.value, num2.value, select.value);
 });
